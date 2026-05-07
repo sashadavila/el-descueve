c5aa281c91ad418611302ddfef6dfd0324b3bfd8
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-06 19:39:38 -0500

    Se incluyen logica de paginas

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
+import { CartProvider } from './store/CartContext'
+import { AuthProvider } from './store/AuthContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
