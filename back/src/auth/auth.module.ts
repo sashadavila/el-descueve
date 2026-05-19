946a53653dca0769a379c83b5127fff6f3167be6
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-19 17:15:33 -0500

    modifica auth module

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -5,19 +5,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
 import { EmailService } from '../common/services/email.service';
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'superSecretKey',
       signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, EmailService],
+  providers: [AuthService, JwtStrategy, GoogleStrategy, EmailService],
   exports: [AuthService],
 })
 export class AuthModule { }
