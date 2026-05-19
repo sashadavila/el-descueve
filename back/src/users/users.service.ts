dfffec5e451fab46694f308ee4a937c6f7ace318
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-18 22:11:56 -0500

    modifica service

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -9,7 +8,7 @@ export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async findByEmail(email: string): Promise<User | null> {
     return this.usersRepository.findOne({
@@ -26,5 +25,14 @@ export class UsersService {
     return this.usersRepository.findOne({
       where: { id },
     });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async update(id: string, userData: Partial<User>): Promise<User | null> {
+    await this.usersRepository.update(id, userData);
+    return this.findById(id);
   }
 }
