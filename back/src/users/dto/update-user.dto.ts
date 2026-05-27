a0f75df34d76e205baecbd528b2b9c9d74f82000
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-27 18:51:35 -0500

    modifica user dto update

--- back/src/users/dto/update-user.dto.ts
+++ back/src/users/dto/update-user.dto.ts
@@ -1,9 +1,29 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsOptional, IsEnum, IsBoolean } from 'class-validator';
+import { IsOptional, IsEnum, IsBoolean, IsString, IsEmail } from 'class-validator';
 import { UserRole } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @IsOptional()
+    @IsEmail()
+    email?: string;
+
+    @IsOptional()
+    @IsString()
+    phone?: string;
+
+    @IsOptional()
+    @IsString()
+    company?: string;
+
+    @IsOptional()
+    @IsString()
+    rut?: string;
+
     @IsOptional()
     @IsEnum(UserRole)
     role?: UserRole;
