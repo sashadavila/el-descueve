b8c213110820fb9773f5fa5d71d4304f2e456b14
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-27 18:49:55 -0500

    modifica jwt

--- back/src/auth/guards/jwt-auth.guard.ts
+++ back/src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    constructor(private reflector: Reflector) {
+        super();
+    }
+
+    canActivate(context: ExecutionContext) {
+        const isPublic = this.reflector.getAllAndOverride<boolean>('isPublic', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        if (isPublic) {
+            return true;
+        }
+
+        console.log('🔒 [JwtAuthGuard] Verificando token...');
+        return super.canActivate(context);
+    }
+
+    handleRequest(err, user, info) {
+        console.log('🔒 [JwtAuthGuard] Error:', err);
+        console.log('🔒 [JwtAuthGuard] User:', user);
+        console.log('🔒 [JwtAuthGuard] Info:', info);
+
+        if (err || !user) {
+            console.log('🔒 [JwtAuthGuard] Error de autenticación:', info?.message || err?.message);
+            throw new UnauthorizedException('Token inválido o no proporcionado');
+        }
+        return user;
+    }
+}
