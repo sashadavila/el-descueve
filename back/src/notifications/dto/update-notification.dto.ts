8de6641aa10a9588e197fae73776756ccfda806b
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-27 22:22:20 -0500

    crea notificaciones dto update

---
+++ back/src/notifications/dto/update-notification.dto.ts
@@ -0,0 +1,10 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateNotificationDto } from './create-notification.dto';
+import { IsEnum, IsOptional } from 'class-validator';
+import { NotificationStatus } from '../entities/notification.entity';
+
+export class UpdateNotificationDto extends PartialType(CreateNotificationDto) {
+    @IsOptional()
+    @IsEnum(NotificationStatus)
+    status?: NotificationStatus;
+}
