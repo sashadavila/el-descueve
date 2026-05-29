775b6b3cf31cc38eed1c298ef187aa726b08c59b
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-05-28 23:44:23 -0500

    modifica product module

--- back/src/products/products.module.ts
+++ back/src/products/products.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
