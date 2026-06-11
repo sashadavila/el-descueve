8e5323da6817e6a3f4abaf64aa6155fd1e3c6a8e
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-06-10 23:25:14 -0500

    modifica product module

--- back/src/products/products.module.ts
+++ back/src/products/products.module.ts
@@ -4,9 +4,10 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity';
 import { CategoriesModule } from '../categories/categories.module';
+import { OrderItem } from '../order-items/entities/order-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
+  imports: [TypeOrmModule.forFeature([Product, OrderItem]), CategoriesModule],
   controllers: [ProductsController],
   providers: [ProductsService],
   exports: [ProductsService],
