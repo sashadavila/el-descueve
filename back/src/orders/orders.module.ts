9dd8d87c77a36fb9da129a01c5e14d161147e80f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-06-12 15:22:36 -0500

    modifica orders module

--- back/src/orders/orders.module.ts
+++ back/src/orders/orders.module.ts
@@ -7,9 +7,12 @@ import { OrdersController } from './orders.controller';
 import { Order } from './entities/order.entity';
 import { OrderItem } from '../order-items/entities/order-item.entity';
 import { Product } from '../products/entities/product.entity';
+import { Shipment } from '../shipments/entities/shipment.entity'; // ← Agregar importación
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem, Product, Shipment]) // ← Agregar Shipment aquí
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
