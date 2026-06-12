49f878dabe310cce5f2c4f9f32f529b3f3986eb1
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-06-11 21:25:23 -0500

    modifica notifications dto update

--- back/src/notifications/dto/update-notification.dto.ts
+++ back/src/notifications/dto/update-notification.dto.ts
@@ -1,3 +1,4 @@
+// src/notifications/dto/update-notification.dto.ts
 import { PartialType } from '@nestjs/swagger';
 import { CreateNotificationDto } from './create-notification.dto';
 import { IsEnum, IsOptional } from 'class-validator';
