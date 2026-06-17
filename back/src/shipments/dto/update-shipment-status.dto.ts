e9bc9cc79d777ce475c259b53c3f1b326c3e4dd9
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-06-16 22:23:54 -0500

    modifica shipment dto update

--- back/src/shipments/dto/update-shipment-status.dto.ts
+++ back/src/shipments/dto/update-shipment-status.dto.ts
@@ -1,10 +1,20 @@
 // src/shipments/dto/update-shipment-status.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 import { ShipmentStatus } from '../entities/shipment.entity';
 
 export class UpdateShipmentStatusDto {
     @ApiProperty({ enum: ShipmentStatus })
     @IsEnum(ShipmentStatus)
     status: ShipmentStatus;
+
+    // ✅ Agregar campos opcionales para evitar errores de whitelist
+    @IsOptional()
+    shippedAt?: Date;
+
+    @IsOptional()
+    deliveredAt?: Date;
+
+    @IsOptional()
+    estimatedDelivery?: Date | string | null;
 }
